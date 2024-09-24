e2c2af6a7703e18d9cb5fb77070d3af16dd5b018
aquinjay <103866978+aquinjay@users.noreply.github.com>
2024-09-24 08:17:08 -0700

    Update distro_startup.fish

--- distro_startup.fish
+++ distro_startup.fish
@@ -46,20 +46,20 @@ set packages git neovim tree bat chezmoi nodejs npm
 pacman -S --needed --noconfirm $packages
 
 # setup chezmoi dotfile
-if not test -d ~/.local/share/chezmoi
-	echo 'Chezmoi init file not available. Initializing file.'
-	chezmoi init https://github.com/aquinjay/dotfiles.git
-else
-	echo 'Chezmoi file already exists.'
-end
+#if not test -d ~/.local/share/chezmoi
+#	echo 'Chezmoi init file not available. Initializing file.'
+#	chezmoi init https://github.com/aquinjay/dotfiles.git
+#else
+#	echo 'Chezmoi file already exists.'
+#end
 
-echo "Setup GitHub Identity"
-git config --global user.email "afquinteroj@pm.me"
-git config --global user.name "aquinjay"
+#echo "Setup GitHub Identity"
+#git config --global user.email "afquinteroj@pm.me"
+#git config --global user.name "aquinjay"
 
 # Create git management directories
-echo "Create Git directories"
-set base_dir $HOME/Documents/git
-mkdir -p $base_dir/GitHub $base_dir/GitLab
+#echo "Create Git directories"
+#set base_dir $HOME/Documents/git
+#mkdir -p $base_dir/GitHub $base_dir/GitLab
 
-echo "Please add ssh agent to fish configuration"
+#echo "Please add ssh agent to fish configuration"
